4c83825ce1650f6b2f550f2c4f79d64b871e5971
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2026-04-28 04:00:23 +0700

    update

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Users\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
@@ -49,6 +50,7 @@ class UsersTable
             ])
             ->recordActions([
                 EditAction::make()->visible(fn($record) => $record->role == 'laboran'),
+                DeleteAction::make()
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
